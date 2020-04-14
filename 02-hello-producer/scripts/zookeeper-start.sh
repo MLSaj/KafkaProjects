f826ba02c68a3d876f23987a9eaf7f49a1753ca2
MLSaj <sajeed.bakht@mail.utoronto.ca>
2020-04-14 16:20:17 -0400

    Initial Commit

---
+++ 02-hello-producer/scripts/zookeeper-start.sh
@@ -0,0 +1,3 @@
+/Users/sajeedbakht/Documents/kafka23/bin/zookeeper-server-start.sh /Users/sajeedbakht/Documents/kafka23/config/zookeeper.properties
+
+/Users/sajeedbakht/Documents/kafka23/bin/zookeeper-shell.sh localhost:2181
